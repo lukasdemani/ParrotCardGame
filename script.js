0b39a53c1fc11f6d59cdd8fe65c044d89a061a1c
lukasdemani <37150803+lukasdemani@users.noreply.github.com>
2021-11-06 22:32:58 +0000

    Add files via upload
    Card images and random function

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
 const cardQuant = prompt("How many cards do you want?");
 
 let divCards = document.querySelector(".cards");
+const imgCard = ["<img src='img/explodyparrot.gif' alt=''>", 
+                "<img src='img/fiestaparrot.gif' alt=''>",
+                "<img src='img/metalparrot.gif' alt=''>",
+                "<img src='img/revertitparrot.gif' alt=''>",
+                "<img src='img/tripletsparrot.gif' alt=''>",
+                "<img src='img/unicornparrot.gif' alt=''>",
+                "<img src='img/front.gif' alt=''>"];
+
+const imgSelection = [];
+
+for (let i=0; i<(cardQuant/2); i++){
+    imgSelection.push(imgCard[i]);
+    imgSelection.push(imgCard[i]);
+};
+
+imgSelection.sort(compare);
+
+function compare() { 
+	return Math.random() - 0.5; 
+}
 
 for (let i = 0; i < cardQuant; i++) {
 
@@ -10,10 +30,10 @@ for (let i = 0; i < cardQuant; i++) {
             <img  src="img/front.png" alt="">
         </div>
         <div class="face back-face">
-            Verso
+            ${imgSelection[i]}
         </div>
     </div>`;
-}
+};
 
 function flipCard(cardClicked) {
     cardClicked.classList.toggle("flip");
