9b15fb3f61c92862f0fda6c349ae9598c50e33ad
lukasdemani <37150803+lukasdemani@users.noreply.github.com>
2021-11-07 00:15:26 +0000

    Add files via upload
    flip just 2 cards in turn and alert win

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const cardQuant = prompt("How many cards do you want?");
+const cardQuant = parseInt(prompt("How many cards do you want?"));
 
 let divCards = document.querySelector(".cards");
 const imgCard = ["<img src='img/explodyparrot.gif' alt=''>", 
@@ -7,9 +7,15 @@ const imgCard = ["<img src='img/explodyparrot.gif' alt=''>",
                 "<img src='img/revertitparrot.gif' alt=''>",
                 "<img src='img/tripletsparrot.gif' alt=''>",
                 "<img src='img/unicornparrot.gif' alt=''>",
-                "<img src='img/front.gif' alt=''>"];
+                "<img src='img/front.png' alt=''>"];
 
 const imgSelection = [];
+
+let flippedCard = 0;
+let firstCard = '';
+let secondCard = '';
+let moves = 0;
+let points = 0;
 
 for (let i=0; i<(cardQuant/2); i++){
     imgSelection.push(imgCard[i]);
@@ -35,7 +41,41 @@ for (let i = 0; i < cardQuant; i++) {
     </div>`;
 };
 
-function flipCard(cardClicked) {
-    cardClicked.classList.toggle("flip");
+function resetCards(){
+    firstCard.classList.toggle("flip");
+    secondCard.classList.toggle("flip");
+    flippedCard = 0;
 };
 
+function flipCard(cardClicked) {
+    if (flippedCard === 0) {
+        firstCard = cardClicked;
+        firstCard.classList.toggle("flip");
+        flippedCard++;
+    }else if (flippedCard===1){
+        secondCard = cardClicked;
+        secondCard.classList.toggle("flip");
+        flippedCard++;
+        if (firstCard.innerHTML !== secondCard.innerHTML){
+            setTimeout(resetCards, 1000);
+        }else{
+            firstCard.onclick = null;
+            secondCard.onclick = null;
+            points++;
+            flippedCard = 0;
+        };
+    };
+    movesCount();
+};
+
+function movesCount(){
+    moves++;
+    console.log(moves);
+    if (points===cardQuant/2){
+        setTimeout(alertWin, 1000);
+    }
+};
+
+function alertWin() {
+    alert(`You win in ${moves} turns!`);
+};
